38244199c0d7dd3aef637eba1328ec6f11f8b220
mika_sampaio <mikaeli.sampaio@sharkdev.com.br>
2026-02-16 15:34:30 -0300

    feat: adicionar estratégia JWT e guard para autenticação de usuários

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
-import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UserModule } from '../users/user.module';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
-    PrismaModule,
+    UserModule,
+    PassportModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
   controllers: [AuthController],
 })
--- src/modules/users/user.controllers.ts
+++ src/modules/users/user.controllers.ts
@@ -5,23 +5,27 @@ import {
   Get,
   Post,
   Put,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from './user.services';
 import { CreateUserDTO, UpdateUserDTO } from './dtos/user.dtos';
 import { LoggingInterceptor } from 'src/shared/interceptors/logging.interceptor';
 import { ParamsId } from 'src/shared/decorators/params.decorator';
+import { JwtAuthGuard } from 'src/shared/guards/jwt.guard';
 
 @UseInterceptors(LoggingInterceptor)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   getUsers() {
     return this.userService.getUsers();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   getById(@ParamsId() id: number) {
     return this.userService.getById(Number(id));
@@ -32,11 +36,13 @@ export class UserController {
     return await this.userService.createUser(body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   updateUser(@ParamsId() id: number, @Body() body: UpdateUserDTO) {
     return this.userService.updateUser(Number(id), body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async deleteUser(@ParamsId() id: number) {
     return await this.userService.deleteUser(Number(id));
